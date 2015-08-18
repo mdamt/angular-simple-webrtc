75cd4a53e6c364ba11b4efab1bdb8b050e5eb393
Mohammad Anwari <mdamt@mdamt.net>
2015-08-18 08:41:32 +0700

    Simple mock

---
+++ tests/simplewebrtcMock.js
@@ -0,0 +1,24 @@
+'use strict';
+var SimpleWebRTCMock = function SimpleWebRTC(options) {
+  var self = this;
+
+  this.S = new OriginalSimpleWebRTC(options);
+}
+
+SimpleWebRTCMock.prototype.emit = function(n, e) {
+  var self = this;
+  self.S.emit(n, e);
+}
+
+SimpleWebRTCMock.prototype.on = function(n, e) {
+  var self = this;
+  self.S.on(n, e);
+}
+
+SimpleWebRTCMock.prototype.joinRoom = function(r) {
+  var self = this;
+  self.S.emit('joinedRoom');
+}
+
+var OriginalSimpleWebRTC = SimpleWebRTC;
+SimpleWebRTC = SimpleWebRTCMock;
